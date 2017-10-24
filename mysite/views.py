c2e9866902d61431865b6db91f5b5f0ed589ef31
OtakuEmperor <g3odshackoe@gmail.com>
2017-10-24 20:51:21 +0800

    move the feature-gitCommitStatus to the web site

--- mysite/views.py
+++ mysite/views.py
@@ -1,20 +1,21 @@
 from django.contrib import auth  # 別忘了import auth
 from django.shortcuts import render_to_response
+from .pushTracker import printUserGitStatus
 def login(request):
 
-    if request.user.is_authenticated(): 
+    if request.user.is_authenticated():
         return HttpResponseRedirect('/index/')
 
     username = request.POST.get('username', '')
     password = request.POST.get('password', '')
-    
+
     user = auth.authenticate(username=username, password=password)
 
     if user is not None and user.is_active:
         auth.login(request, user)
         return HttpResponseRedirect('/index/')
     else:
-        return render_to_response('login.html') 
+        return render_to_response('login.html')
 
 def index(request):
     return render_to_response('index.html',locals())
@@ -22,3 +23,9 @@ def index(request):
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect('/index/')
+
+def gitCommitYet(request):
+    result = printUserGitStatus()
+    return render_to_response('whyIndexIsNotInTheRootDirButInIndexDir.html',{
+        'gitStatus' : result
+    })
